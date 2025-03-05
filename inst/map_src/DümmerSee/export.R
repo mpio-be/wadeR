1509286c1dec1100389aaf9fb09401b309f5ff72
Mihai Valcu <valcu@orn.mpg.de>
2025-03-05 13:20:01 +0100

    -

--- inst/map_src/DümmerSee/export.R
+++ inst/map_src/DümmerSee/export.R
@@ -3,8 +3,11 @@
 require(sf)
 require(dplyr)
 
-x = st_read("./inst/map_src/DümmerSee/OsterFeinerMoor.kml")
-OsterFeinerMoor = select(x, Name) |> rename(id = Name)
+x = st_read("./inst/map_src/DümmerSee/OsterFeinerMoor.kml") |> st_zm()
+OsterFeinerMoor =
+  select(x, Name) |>
+  rename(id = Name) |>
+  st_make_valid()
 
 
 usethis::use_data(OsterFeinerMoor, overwrite = TRUE)
